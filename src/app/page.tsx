b69b52466ffd3ad1e757b63a7993042530c6c5d1
minhduc2611 <minhduc261298@gmail.com>
2024-04-07 12:42:47 +0700

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,37 @@
 import { useClientAuthStore } from "@/stores/authentication";
 import "dayjs/locale/en";
 import Link from "next/link";
+import { useDeviceSelectors } from "react-device-detect";
 import ReactTypingEffect from "react-typing-effect";
 import { twMerge } from "tailwind-merge";
-import { isMobile, isTablet } from "react-device-detect";
 
 export default function Home() {
   const { user } = useClientAuthStore();
+  const [selectors] = useDeviceSelectors(window.navigator.userAgent)
+
+  const { isMobile } = selectors
   return (
     <main
       className={twMerge(
-        "flex min-h-screen items-center justify-center gap-52 p-10 md:p-20 h-full bg-gray-800 text-gray-100"
+        "flex w-full min-h-screen items-center justify-center gap-52 p-10 md:p-20 h-full bg-gray-800 text-gray-100"
       )}
     >
+      {!user && (
+        <Link
+          href="/login"
+          className={twMerge(
+            "absolute text-gray-100 text-[1rem] rounded",
+            isMobile ? "bottom-16 animate-pulse" : "top-10 right-10"
+          )}
+        >
+          Join us
+        </Link>
+      )}
       <div
-        className={
-          (twMerge("text-center"),
-          isMobile || isTablet ? "text-[3rem]" : "text-[6rem]")
-        }
+        className={twMerge(
+          "text-center",
+          isMobile ? "text-[2.5rem]" : "text-[6rem]"
+        )}
       >
         <p>We build</p>
         <p>Something</p>
@@ -26,11 +40,6 @@ export default function Home() {
           text={["Extraordinary", "Robust", "Extremely Fast"]}
         />
       </div>
-      {!user && (
-        <Link href="/login" className="fixed top-10 right-10 text-gray-100">
-          Join us
-        </Link>
-      )}
     </main>
   );
 }
