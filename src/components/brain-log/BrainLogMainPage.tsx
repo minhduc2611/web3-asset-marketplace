420c953fe4cedb6992db071717ae7ca3a2115dd8
minhduc2611 <minhduc261298@gmail.com>
2024-04-07 15:00:24 +0700

    add

---
+++ src/components/brain-log/BrainLogMainPage.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { BrainLogList } from "@/components/brain-log/BrainLogList";
+import { useClientAuthStore } from "@/stores/authentication";
+import { useBrainLogStoreActions } from "@/stores/brainLog";
+import { redirect } from "next/navigation";
+
+import { useEffect } from "react";
+
+export default function BrainLogMainPage() {
+  const { user } = useClientAuthStore();
+  if(!user) {
+    redirect("/login");
+  }
+  const { getBrainLogTypes } = useBrainLogStoreActions(user.id);
+
+  useEffect(() => {
+    console.log("getBrainLogTypes", user);
+    if (user) {
+      getBrainLogTypes(user?.id);
+    }
+  }, [user]);
+
+  return (
+    <main className="min-h-screen p-10">
+      <BrainLogList />
+    </main>
+  );
+}
