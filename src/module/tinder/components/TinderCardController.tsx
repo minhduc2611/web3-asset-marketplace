84e5b7402d634e77f41eeab43c5de21cadf099e8
minhduc2611 <minhduc261298@gmail.com>
2024-04-21 01:41:58 +0700

    Add

--- src/module/tinder/components/TinderCardController.tsx
+++ src/module/tinder/components/TinderCardController.tsx
@@ -5,7 +5,7 @@ import { useTinderStore } from "../store/tinder";
 const TinderCardController = () => {
   const store = useTinderStore();
   return (
-    <div>
+    <div className="h-40">
       <div className="h-24 flex items-center gap-6 justify-center">
         <Icons.tinderUnlike className="hover:scale-105 active:animate-[wiggle_1s_ease-in-out_infinite]" />
         <Icons.tinderSuperLike className="hover:scale-105 active:animate-[wiggle_1s_ease-in-out_infinite]" />
