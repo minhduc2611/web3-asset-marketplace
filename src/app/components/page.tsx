e4143c0f48746618e982626528a20429a72af98c
minhduc2611 <minhduc261298@gmail.com>
2024-10-22 12:53:28 +0300

    add

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -14,6 +14,7 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 import CommonAudioUploader from "@/components/common/common-audio-uploader";
 import { useLocalStorage } from "usehooks-ts";
 import useSound from "use-sound";
+import { useRef } from "react";
 
 export default function Home() {
   const { user } = useClientAuthStore();
@@ -28,21 +29,29 @@ export default function Home() {
     { title: `CommonAudioUploader`, content: <CommonAudioUploader /> },
   ];
 
-  // {
-  //   sound: Howl | null;
-  //   stop: (id?: string) => void;
-  //   pause: (id?: string) => void;
-  //   duration
-  const [play, { sound, duration }] = useSound(
-    "https://skvnrwmwmcvsevknedhm.supabase.co/storage/v1/object/public/flashcard/audio/file_1727614514685_e7n172.mp3"
-  );
   return (
     <main className="min-h-screen p-24 md:px-24">
-      <button onClick={() => play()}>Play</button>
-      <p>
+      <button
+        className="p-4 rounded-sm border border-stone-900"
+        type="button"
+        onClick={() => {
+          console.log("Play");
+          const audio = new Audio();
+          // audio.autoplay = true;
+          audio.src =
+            "https://skvnrwmwmcvsevknedhm.supabase.co/storage/v1/object/public/flashcard/audio/file_1727614514685_e7n172.mp3";
+          audio.load();
+          setTimeout(() => {
+            audio.play();
+          }, 1000);
+        }}
+      >
+        Play
+      </button>
+      {/* <p>
         {sound?.state()}
         {duration}
-      </p>
+      </p> */}
       {/* <TabView
         activeIndex={activeIndex}
         onTabChange={(e) => setActiveIndex(e.index)}
