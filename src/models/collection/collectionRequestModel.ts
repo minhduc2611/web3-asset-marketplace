efb91d61ad4e6a142d85f346260ea82e0dc979c4
minhduc2611 <minhduc261298@gmail.com>
2024-10-26 16:48:45 +0300

    add

--- src/models/collection/collectionRequestModel.ts
+++ src/models/collection/collectionRequestModel.ts
@@ -1,3 +1,3 @@
 import { CollectionModel } from "./collectionModel";
 
-export type CollectionAddRequestModel = Omit<CollectionModel, 'id' | 'deleted_at' | 'created_at'>
+export type CollectionAddRequestModel = Omit<CollectionModel, 'id' | 'deleted_at' | 'created_at' | 'shared_author_ids'>
